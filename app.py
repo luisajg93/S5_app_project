e147e4904fdb4fe22ee2a11aad9c4fc18187b42c
Luis Jimenez <lbtojg@gmail.com>
2024-06-21 11:36:07 -0600

    se modificaron acentos y se guardaron los cambios

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicaci¢n a£n no es funcional. En construcci¢n.')
+st.write('Esta aplicaci√≥n a√∫n no es funcional. En construcci√≥n.')
